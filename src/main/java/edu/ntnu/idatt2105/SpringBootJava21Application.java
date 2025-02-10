9b25230777caee5ab94b2921ca458576362b46fe
Segward <gustask@stud.ntnu.no>
2025-02-10 16:58:27 +0100

    First commit

---
+++ src/main/java/edu/ntnu/idatt2105/SpringBootJava21Application.java
@@ -0,0 +1,11 @@
+package edu.ntnu.idatt2105;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJava21Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJava21Application.class, args);
+    }
+}

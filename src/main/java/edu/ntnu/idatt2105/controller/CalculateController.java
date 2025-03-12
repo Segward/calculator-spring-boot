07c74fbcc47b6dce170dc1aee0df5d398ed1a27c
Segward <gustask@stud.ntnu.no>
2025-03-12 13:53:18 +0100

    Requested mapping to api

--- src/main/java/edu/ntnu/idatt2105/controller/CalculateController.java
+++ src/main/java/edu/ntnu/idatt2105/controller/CalculateController.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 @CrossOrigin(origins = "http://localhost:5173")
 public class CalculateController {
 

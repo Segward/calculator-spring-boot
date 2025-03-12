f0e81c8ccf6744f09afdc3cf6e5021361f1ec5e3
Segward <gustask@stud.ntnu.no>
2025-03-12 13:31:20 +0100

    Removed unused imports

--- src/main/java/edu/ntnu/idatt2105/controller/CalculateController.java
+++ src/main/java/edu/ntnu/idatt2105/controller/CalculateController.java
@@ -1,6 +1,5 @@
 package edu.ntnu.idatt2105.controller;
 
-import edu.ntnu.idatt2105.dao.HistoryDAO;
 import edu.ntnu.idatt2105.models.CalculateResponse;
 import edu.ntnu.idatt2105.services.CalculateService;
 import java.util.logging.Logger;
@@ -8,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 

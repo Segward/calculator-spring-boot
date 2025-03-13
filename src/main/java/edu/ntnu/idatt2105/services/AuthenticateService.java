f09c7518ebd83b7836c1b623577b1bb7ece259d3
Segward <gustask@stud.ntnu.no>
2025-03-13 16:52:16 +0100

    Checks length of username and password or empty value

--- src/main/java/edu/ntnu/idatt2105/services/AuthenticateService.java
+++ src/main/java/edu/ntnu/idatt2105/services/AuthenticateService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 public class AuthenticateService {
 
   public TokenResponse getToken(String username, String password) throws Exception {
-    if (username == null || password == null || username.length() < 1) {
+    if (username == null || password == null || username.length() < 1 || password.length() < 1) {
       throw new Exception("Username or password is null or empty");
     }
     boolean exists = UserDAO.exists(username);

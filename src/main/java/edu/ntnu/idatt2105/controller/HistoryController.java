f738bca96bf2c0a9b640c10c3d1abac455618964
Segward <gustask@stud.ntnu.no>
2025-03-13 16:18:26 +0100

    Removed unused imports

--- src/main/java/edu/ntnu/idatt2105/controller/HistoryController.java
+++ src/main/java/edu/ntnu/idatt2105/controller/HistoryController.java
@@ -5,7 +5,6 @@ import edu.ntnu.idatt2105.services.HistoryService;
 import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
